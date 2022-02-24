55f15ab7d54baac78ed4cfd0d3d1c48a34f89cff
Omar Faruk <omarfarukesham@gmail.com>
2022-02-24 11:41:41 +0600

    dyanmic search done

--- js/api.js
+++ js/api.js
@@ -6,27 +6,27 @@ searchFood = () =>{
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${getValue}`
     fetch(url)
     .then(res => res.json())
-    .then(data => displayInfo(data))
+    .then(data => displayInfo(data.meals))
 }
 
 const displayInfo = (data) =>{
-    console.log(data.meals[0])
-    // const isIamges = `<img src="${data.meals[0].strMealThumb}" />`
-    // console.log(isIamges)
+    console.log(data)
+ 
+    
     const getArea = document.getElementById('getDynamicInfo')
-   //for(const foodInfo of data){}
+    for(const foodInfo of data){
        const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = ` 
-             <div class="card w-75">
-                <img class="img-fluid" src="${data.meals[0].strMealThumb}" alt="...">
+             <div class="card">
+                <img class="img-fluid" src="${foodInfo.strMealThumb}" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${data.meals[0].strMeal}</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 class="card-title">${foodInfo.strMeal}</h5>
+                  <p class="card-text">${foodInfo.strInstructions.slice(0,200)}</p>
                 </div>
               </div>
        `
        getArea.appendChild(div)
    
-
+    }
 }

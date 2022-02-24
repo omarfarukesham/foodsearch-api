847610feed86330398f4592a7837a815398786a3
Omar Faruk <omarfarukesham@gmail.com>
2022-02-24 12:31:33 +0600

    youtube button done

--- js/api.js
+++ js/api.js
@@ -10,15 +10,13 @@ searchFood = () =>{
 }
 
 const displayInfo = (data) =>{
-    console.log(data)
- 
-    
+    //console.log(data)
     const getArea = document.getElementById('getDynamicInfo')
     for(const foodInfo of data){
        const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = ` 
-             <div class="card">
+             <div onclick="loadProductDetails('${foodInfo.idMeal}')" class="card">
                 <img class="img-fluid" src="${foodInfo.strMealThumb}" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${foodInfo.strMeal}</h5>
@@ -30,3 +28,31 @@ const displayInfo = (data) =>{
    
     }
 }
+
+const loadProductDetails = (mealId) =>{
+   // console.log(mealId)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => productDetails(data.meals[0]))
+
+}
+
+const productDetails = (meal)=>{
+    //console.log(meal)
+    const getMealDetailsArea = document.getElementById('detailProInfo')
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+                <img src="${meal.strMealThumb} " class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${meal.strMeal}</h5>
+                  <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+                  <a href="${meal.strYoutube}" target="-blank" class="btn btn-primary">Go Youtube</a>
+                </div>
+    `
+    getMealDetailsArea.appendChild(div)
+}
+
+
+
